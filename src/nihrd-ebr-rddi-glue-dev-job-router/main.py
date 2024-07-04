ae38d7bfefcbfdad5fce6e5875edbb830f114353
eunanhardypa <50829217+eunanhardypa@users.noreply.github.com>
2024-07-04 12:03:29 +0100

    First pass at ETL trigger lambda

---
+++ src/nihrd-ebr-rddi-glue-dev-job-router/main.py
@@ -0,0 +1,40 @@
+import boto3
+import os
+import logging
+
+def lambda_handler(event,ctx):
+    glue = boto3.client('glue')
+    log = logging.getLogger()
+    log.setLevel(logging.INFO)
+    # TODO implement
+    try:
+        bucketName = event["detail"]["bucket"]["name"]
+        keyName = event["detail"]["object"]["key"]
+        inputPath = f"s3://{bucketName}/{keyName}"
+        if os.environ.get('TARGET_BUCKET') is not None:
+            outputPath = f"s3://{os.environ.get('TARGET_BUCKET')}/{keyName}"
+        else:
+            raise Exception("Target bucket not set")
+        
+        if os.environ.get("JOB_NAME") is not None:
+            jobName = os.environ.get("JOB_NAME")
+        else:
+            raise Exception("Job name is not set correctly")
+            
+
+        job_parameters = {
+            '--input_path': inputPath,
+            '--output_path': outputPath
+        }
+        
+        response = glue.start_job_run(JobName=jobName,Arguments=job_parameters)
+        return {
+            'statusCode': 200,
+            'body': "OK"
+        }
+    except Exception as e:
+        print(e + "\n")
+        return {
+            'statusCode':500,
+            'body': e
+        }
