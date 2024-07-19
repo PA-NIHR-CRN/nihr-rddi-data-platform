957f299e3086ed16e8f4b4e1511ca8559ac7515f
eunanhardypa <50829217+eunanhardypa@users.noreply.github.com>
2024-07-19 11:56:06 +0100

    Updating Output path

--- src/glue-router/main.py
+++ src/glue-router/main.py
@@ -1,17 +1,21 @@
 import boto3
 import os
 import logging
+import datetime
+
 
 def lambda_handler(event,ctx):
     glue = boto3.client('glue')
     log = logging.getLogger()
     log.setLevel(logging.INFO)
+    # TODO implement
     try:
         bucketName = event["detail"]["bucket"]["name"]
         keyName = event["detail"]["object"]["key"]
         inputPath = f"s3://{bucketName}/{keyName}"
         if os.environ.get('TARGET_BUCKET') is not None:
-            outputPath = f"s3://{os.environ.get('TARGET_BUCKET')}/{keyName}/"
+            currentTime = datetime.datetime.now()
+            outputPath = f"s3://{os.environ.get('TARGET_BUCKET')}/{currentTime.year}/{currentTime.month}/{currentTime.day}/{currentTime.hour}/"
         else:
             raise Exception("Target bucket not set")
         
@@ -19,11 +23,11 @@ def lambda_handler(event,ctx):
             jobName = os.environ.get("JOB_NAME")
         else:
             raise Exception("Job name is not set correctly")
-            
+        
 
         job_parameters = {
             '--input_path': inputPath,
-            '--output_path': outputPath
+            '--output_path': outputPath,
         }
         
         response = glue.start_job_run(JobName=jobName,Arguments=job_parameters)
